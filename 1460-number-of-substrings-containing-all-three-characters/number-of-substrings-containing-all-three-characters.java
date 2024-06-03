7cdda1dbb92170b24e60b48f7e3937097d3019df
yashraj <yashraj9940@gmail.com>
2024-06-04 02:23:52 +0530

    Time: 23 ms (24.96%) | Memory: 44.9 MB (17.18%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -6,9 +6,9 @@ class Solution {
         while(i<s.length()){
             ar[s.charAt(i)-'a']=i;
              int min = Collections.min(Arrays.asList(ar));
-            if(min>=0){
+      
                 ans+=min+1;
-            }
+       
             i++;
         }
         return ans;
