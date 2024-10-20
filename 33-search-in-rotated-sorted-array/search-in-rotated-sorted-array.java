ad4289b650066631dddd497afca464ef46fadb0b
yashraj <yashraj9940@gmail.com>
2024-10-20 12:29:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (13.23%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,13 +1,31 @@
 class Solution {
     public int search(int[] nums, int target) {
-        HashMap<Integer,Integer> resultmap=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            resultmap.put(nums[i],i);
+        
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] >= nums[left]) {
+                if (nums[left] <= target && target <= nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
         }
-        if (resultmap.containsKey(target)){
-            return resultmap.get(target);
-        }else{
-            return -1;
-        }
-    }
+
+        return -1;        
+   
+
+     }
 }
