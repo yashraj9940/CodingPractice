982665b72652e79aa167013906123eefec85e3d8
yashraj <yashraj9940@gmail.com>
2024-10-20 21:25:49 +0530

    Time: 34 ms (37.90%) | Memory: 52.5 MB (18.94%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,47 +1,41 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> ans=new ArrayList<List<Integer>>();
         int len=nums.length;
+        List<List<Integer>> ans=new ArrayList<List<Integer>>();
         for(int i=0;i<len-2;i++){
-           
+           int j=i+1,k=len-1;
+           while(j<k){
+               int sum=nums[i]+nums[j]+nums[k];
+               if(sum==0){
+                     ArrayList<Integer> a=new ArrayList<>();
+                     a.add(nums[i]); a.add(nums[j]);a.add(nums[k]);
+                     ans.add(a);
 
-            int l=i+1,h=len-1;;
-            while(l<h && nums[i]<=0){
-                int sum=nums[i]+nums[l]+nums[h];
-                if(sum==0 ){
-                   List<Integer> a=new ArrayList<Integer>();
-                   //a.add(i);a.add(l);a.add(h);
-                     a.add(nums[i]);a.add(nums[l]);a.add(nums[h]);
-                  //   if(!ans.contains(a))
-                   ans.add(a);
-
-                    // Skip duplicate elements for j
-                    while (l < h && nums[l] == nums[l + 1]) {
-                        l++;
+                      // Skip duplicate elements for j
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
                     }
 
                     // Skip duplicate elements for k
-                    while (l < h && nums[h] == nums[h - 1]) {
-                        h--;
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
                     }
-                   l++;
-                   h--; continue;                  
-                }
-                else if(sum>0){
-                        h--;
-                }
-                else{
-                    l++;
-                }           
-            }   
-
-              while (i<len-2 && nums[i] == nums[i + 1]) {
+                   j++;
+                   k--; continue;      
+               }
+               else if(sum<0){
+                j++;
+               }
+               else{
+                k--;
+               }
+            }  while (i<len-2 && nums[i] == nums[i + 1]) {
                 i++;
-            }
-
-        }
-        return ans;
+            }  
+                      }
+                           return ans;
+       }
+   
         
     }
-}
