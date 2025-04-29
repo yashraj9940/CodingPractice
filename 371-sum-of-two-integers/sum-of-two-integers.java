de126d6f14e8da04f7955d23c854a771943fe5ca
yashraj <yashraj9940@gmail.com>
2025-04-30 04:36:40 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (61.03%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.java
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -1,10 +1,27 @@
 class Solution {
-    public int getSum(int a, int b) {
-        while(b != 0){
-            int temp = (a&b)<<1;
-            a = a ^ b;
-            b = temp;
+  public int getSum(int a, int b) {
+        if(a == 0) {
+            return b;
         }
+        
+        if(b == 0) {
+            return a;
+        }
+        
+        int carry = 0;
+        
+        while(b != 0) {
+            
+            // If both bits are 1, we set the bit to the left (<<1) to 1 -- this is the carry step
+            carry = (a & b) << 1;
+            
+            // If both bits are 1, this will give us 0 (we will have a carry from the step above)
+            // If only 1 bit is 1, this will give us 1 (there is nothing to carry)
+            a = a ^ b;
+            
+            b = carry;
+        }
+        
         return a;
     }
 }
