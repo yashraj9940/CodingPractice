b86c8044376d6cfb2f87c2c997e98f4fa2ba2489
yashraj <yashraj9940@gmail.com>
2025-05-20 00:51:11 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (16.55%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,39 +1,49 @@
-public class Solution {
-public int[] searchRange(int[] nums, int target) {
-    int[] result = new int[2];
-    result[0] = findFirst(nums, target);
-    result[1] = findLast(nums, target);
-    return result;
-}
-
-private int findFirst(int[] nums, int target){
-    int idx = -1;
-    int start = 0;
-    int end = nums.length - 1;
-    while(start <= end){
-        int mid = (start + end) / 2;
-        if(nums[mid] >= target){
-            end = mid - 1;
-        }else{
-            start = mid + 1;
-        }
-        if(nums[mid] == target) idx = mid;
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return new int[]{first, last};
     }
-    return idx;
-}
 
-private int findLast(int[] nums, int target){
-    int idx = -1;
-    int start = 0;
-    int end = nums.length - 1;
-    while(start <= end){
-        int mid = (start + end) / 2;
-        if(nums[mid] <= target){
-            start = mid + 1;
-        }else{
-            end = mid - 1;
+    private int findFirst(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int first = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (mid == 0 || nums[mid - 1] != target) {
+                    first = mid;
+                    break;
+                } else {
+                    right = mid - 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-        if(nums[mid] == target) idx = mid;
+        return first;
     }
-    return idx;
-}}
+
+    private int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int last = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (mid == nums.length - 1 || nums[mid + 1] != target) {
+                    last = mid;
+                    break;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return last;
+    }
+}
