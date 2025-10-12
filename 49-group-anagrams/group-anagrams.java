65d95258a49317ee9ea8be14d1d99d00a05a98ee
yashraj <yashraj9940@gmail.com>
2025-10-13 03:39:44 +0530

    Time: 8 ms (38.16%) | Memory: 47.5 MB (95.96%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,32 +1,32 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 class Solution {
-    public String getSignature(String s) {
-        int[] count = new int[26];
-        for (char c : s.toCharArray()) {
-            count[c - 'a']++;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) {
-                sb.append((char) ('a' + i)).append(count[i]);
+    public List<List<String>> groupAnagrams(String[] words) {
+        // Map to store anagram groups
+        Map<String, List<String>> anagramGroups = new HashMap<>();
+        
+        // Iterate through each word in the input array
+        for (String word : words) {
+            // Sort the characters in the word to create a key
+            char[] charArray = word.toCharArray();
+            Arrays.sort(charArray);
+            String key = new String(charArray);
+            
+            // If key is not in the map, add a new entry with the word as a list
+            if (!anagramGroups.containsKey(key)) {
+                anagramGroups.put(key, new ArrayList<>(List.of(word)));
+            }
+            // If key is already present, append the word to the existing list
+            else {
+                anagramGroups.get(key).add(word);
             }
         }
-        return sb.toString();
-    }
-
-    public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<>();
-        Map<String, List<String>> groups = new HashMap<>();
-
-        for (String s : strs) {
-            groups.computeIfAbsent(getSignature(s), k -> new ArrayList<>()).add(s);
-        }
-
-        result.addAll(groups.values());
-
-        return result;
+        
+        // Convert map values to a list and return
+        return new ArrayList<>(anagramGroups.values());
     }
 }
-
