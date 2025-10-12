e74debaf619a8252aab22bcb7aeda4bc2e8ae787
yashraj <yashraj9940@gmail.com>
2025-10-13 04:12:37 +0530

    Time: 12 ms (82.56%) | Memory: 48.9 MB (43.82%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,60 +1,37 @@
+class Node {
+   Integer val;
+   Integer count;
+   Node(){}
+   Node(Integer val,Integer count){
+       this.val=val;
+       this.count=count;
+   }
+    Node(Integer val){
+        this.val=val;
+    }
+
+        
+    }
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // Step 1: Count frequencies
-        Map<Integer, Integer> freqMap = new HashMap<>();
-        for (int n : nums) {
-            freqMap.put(n, freqMap.getOrDefault(n, 0) + 1);
+        PriorityQueue<Node> a=new PriorityQueue<Node>((aa,b)->( b.count-aa.count));
+        Map<Integer,Integer> b=new HashMap<Integer,Integer>();
+        for(Integer i:nums){
+            b.put(i,(b.getOrDefault(i,0)+1));
         }
-
-        // Step 2: Put unique elements into an array
-        int n = freqMap.size();
-        int[] unique = new int[n];
-        int idx = 0;
-        for (int key : freqMap.keySet()) {
-            unique[idx++] = key;
+        for(int i: b.keySet()){
+            a.offer(new Node(i,b.get(i)));
+       //       System.out.println("i="+i+"a="+a);
         }
-
-        // Step 3: Quickselect to partition by frequency
-        quickSelect(unique, 0, n - 1, n - k, freqMap);
-
-        // Step 4: Collect top k
-        return Arrays.copyOfRange(unique, n - k, n);
-    }
-
-    private void quickSelect(int[] unique, int left, int right, int kSmallest, Map<Integer, Integer> freqMap) {
-        if (left >= right) return;
-
-        Random rand = new Random();
-        int pivotIndex = left + rand.nextInt(right - left + 1);
-        pivotIndex = partition(unique, left, right, pivotIndex, freqMap);
-
-        if (pivotIndex == kSmallest) {
-            return;
-        } else if (pivotIndex < kSmallest) {
-            quickSelect(unique, pivotIndex + 1, right, kSmallest, freqMap);
-        } else {
-            quickSelect(unique, left, pivotIndex - 1, kSmallest, freqMap);
+        int[] ans=new int[k];
+        int i=0;
+       //  System.out.println("i="+i+"ss="+a.isEmpty()+"aa"+((i>=k) && (!a.isEmpty())));
+        while((i<k) && (!a.isEmpty())){
+            ans[i]=a.poll().val;
+            i++;
+         //   System.out.println("a="+ans[i]);
         }
-    }
-
-    private int partition(int[] unique, int left, int right, int pivotIndex, Map<Integer, Integer> freqMap) {
-        int pivotFreq = freqMap.get(unique[pivotIndex]);
-        swap(unique, pivotIndex, right);
-        int storeIndex = left;
-
-        for (int i = left; i < right; i++) {
-            if (freqMap.get(unique[i]) < pivotFreq) {
-                swap(unique, storeIndex, i);
-                storeIndex++;
-            }
-        }
-        swap(unique, storeIndex, right);
-        return storeIndex;
-    }
-
-    private void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
+        return ans;
     }
 }
