65888d414062e40ebb49016ce8acabc713ed7aa2
yashraj <yashraj9940@gmail.com>
2025-10-29 04:49:21 +0530

    Time: 17 ms (36.86%) | Memory: 45.6 MB (52.87%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
@@ -9,7 +9,9 @@ class Solution {
        Set<Integer> set = new HashSet<>();
 
     int c=0;
-        for(int i=0;i<26;i++){
+
+    // method 1
+ /*       for(int i=0;i<26;i++){
             if(freq[i]==0)
                 continue;
             if(set.contains(freq[i])){
@@ -21,7 +23,23 @@ class Solution {
             {
                set.add(freq[i]); 
             }
+        } */
+        // method 2
+        Arrays.sort(freq);
+        for(int i=24;i>=0;i--){
+            if(freq[i]==0)
+            break;
+            if(freq[i]>=freq[i+1]){
+                int old=freq[i];
+                int newF=Math.max(freq[i+1]-1,0);
+                c+=old-newF;
+                freq[i]=newF;
+            }
         }
+
+        for(int i=25;i>=0;i--)
+        System.out.println(freq[i]);
+
         return c;
     }
 }
