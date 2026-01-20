a83f6ead3b2baa93418212d8d670d5c6c74cc221
yashraj <yashraj9940@gmail.com>
2026-01-21 04:51:20 +0530

    Time: 28 ms (95.50%) | Memory: 59.2 MB (52.42%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,41 +1,33 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        int len=nums.length;
-        List<List<Integer>> ans=new ArrayList<List<Integer>>();
-        for(int i=0;i<len-2 && nums[i]<=0;i++){
-           int j=i+1,k=len-1;
-           while(j<k){
-               int sum=nums[i]+nums[j]+nums[k];
-               if(sum==0){
-                     ArrayList<Integer> a=new ArrayList<>();
-                     a.add(nums[i]); a.add(nums[j]);a.add(nums[k]);
-                     ans.add(a);
 
-                      // Skip duplicate elements for j
-                    while (j < k && nums[j] == nums[j + 1]) {
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
+
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
+                } else {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+
+                    while (nums[j] == nums[j-1] && j < k) {
                         j++;
                     }
-
-                    // Skip duplicate elements for k
-                    while (j < k && nums[k] == nums[k - 1]) {
-                        k--;
-                    }
-                   j++;
-                   k--; continue;      
-               }
-               else if(sum<0){
-                j++;
-               }
-               else{
-                k--;
-               }
-            }  while (i<len-2 && nums[i] == nums[i + 1]) {
-                i++;
-            }  
-                      }
-                           return ans;
-       }
-   
-        
+                }
+            }
+        }
+        return res;        
     }
+}
