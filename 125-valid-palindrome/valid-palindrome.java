24bfc9b0d5bccfbabac025446b6938472208405e
yashraj <yashraj9940@gmail.com>
2026-08-11 23:50:20 +0530

    Time: 2 ms (99.33%) | Memory: 44.2 MB (83.82%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,31 +1,37 @@
 class Solution {
     public boolean isPalindrome(String s) {
-      if(s.length()<=1)
-         return true; 
-      int i=0,j=s.length()-1;
-      while(i<j){
-        if(!valid(s.charAt(i)))
-        {
-            i++;
-            continue;
-        } if(!valid(s.charAt(j)))
-        {
-            j--;
-            continue;
-        }
-        if(!(s.charAt(i)+"").equalsIgnoreCase(s.charAt(j)+""))
-            return false;
-        i++;j--;
-      } 
-      return true;
+        if (s == null || s.length() <= 1) return true;
 
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            // Skip non-alphanumeric from left
+            while (i < j && !valid(s.charAt(i))) i++;
+            // Skip non-alphanumeric from right
+            while (i < j && !valid(s.charAt(j))) j--;
+
+            // Compare lowercase characters
+            char left = toLower(s.charAt(i));
+            char right = toLower(s.charAt(j));
+            if (left != right) return false;
+
+            i++;
+            j--;
+        }
+        return true;
     }
 
-    public boolean valid(char a){
-        if((a>='a' && a<='z')||(a>='A' && a<='Z')||(a>='0' && a<='9'))
-            return true;
+    // Custom alphanumeric check
+    private boolean valid(char a) {
+        return (a >= 'a' && a <= 'z') ||
+               (a >= 'A' && a <= 'Z') ||
+               (a >= '0' && a <= '9');
+    }
 
-        return false;
-
+    // Custom lowercase conversion
+    private char toLower(char a) {
+        if (a >= 'A' && a <= 'Z') {
+            return (char)(a - 'A' + 'a');
+        }
+        return a;
     }
 }
