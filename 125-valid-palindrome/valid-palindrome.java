d9125c6481983b5a3e02ac7928ec27711aa0e151
yashraj <yashraj9940@gmail.com>
2026-08-11 23:46:18 +0530

    Time: 3 ms (57.92%) | Memory: 46.9 MB (15.03%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,120 +1,31 @@
-// class Solution {
-//     public boolean isPalindrome(String s) {
-
-        // s = s.toLowerCase();
-        // StringBuilder sb = new StringBuilder();
-
-        // for(int i=0; i<s.length(); i++){
-        //     if((s.charAt(i) >= 97 && s.charAt(i) <=122) || (s.charAt(i) >=48 && s.charAt(i)<=57) ){
-        //         sb.append(s.charAt(i));
-        //     }
-        // }
-
-        // for(int i=0; i<sb.length()/2; i++){
-        //     if(sb.charAt(i) != sb.charAt(sb.length() - 1 - i)){
-        //     return false;
-        //     }
-        // }
-        // return true;
-
-
-//         int left = 0, right = s.length() - 1;
-
-//         while (left < right) {
-//             // Skip non-alphanumeric characters
-//             while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-//                 left++;
-//             }
-//             while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-//                 right--;
-//             }
-
-//             // Compare characters (ignore case)
-//             if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
-//                 return false;
-//             }
-
-//             left++;
-//             right--;
-//         }
-
-//         return true;
-//     }
-// }
-
-
-
-
-// class Solution {
-//     public boolean isPalindrome(String s) {
-//         int left = 0;
-//         int right = s.length() - 1;
-
-//         while (left < right) {
-//             // Skip non-letter and non-digit characters
-//             if (!Character.isLetterOrDigit(s.charAt(left))) {
-//                 left++;
-//                 continue;
-//             }
-//             if (!Character.isLetterOrDigit(s.charAt(right))) {
-//                 right--;
-//                 continue;
-//             }
-
-//             // Compare after converting both to lowercase
-//             if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
-//                 return false;
-//             }
-
-//             left++;
-//             right--;
-//         }
-
-//         return true;
-//     }
-// }
-
-
-
 class Solution {
     public boolean isPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
-
-        while (left < right) {
-            char l = s.charAt(left);
-            char r = s.charAt(right);
-
-            // skip non-alphanumeric from left
-            if (!isAlphaNum(l)) {
-                left++;
-                continue;
-            }
-
-            // skip non-alphanumeric from right
-            if (!isAlphaNum(r)) {
-                right--;
-                continue;
-            }
-
-            // compare lowercase versions
-            if (toLower(l) != toLower(r)) {
-                return false;
-            }
-
-            left++;
-            right--;
+      if(s.length()<=1)
+         return true; 
+      int i=0,j=s.length()-1;
+      while(i<j){
+        if(!valid(s.charAt(i)))
+        {
+            i++;
+            continue;
+        } if(!valid(s.charAt(j)))
+        {
+            j--;
+            continue;
         }
+        if(!(s.charAt(i)+"").equalsIgnoreCase(s.charAt(j)+""))
+            return false;
+        i++;j--;
+      } 
+      return true;
 
-        return true;
     }
 
-    // Faster than Character.isLetterOrDigit()
-    private boolean isAlphaNum(char c) {
-        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
-    }
+    public boolean valid(char a){
+        if((a>='a' && a<='z')||(a>='A' && a<='Z')||(a>='0' && a<='9'))
+            return true;
 
-    // Faster than Character.toLowerCase()
-    private char toLower(char c) {
-        return (c >= 'A' && c <= 'Z') ? (char)(c + 32) : c;
+        return false;
+
     }
 }
